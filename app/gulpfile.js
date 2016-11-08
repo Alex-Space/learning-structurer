40661eccbb5bb53d8f65a7ad26d68971c87c4690
AlexSpace <cometospace689@gmail.com>
2016-11-09 00:11:06 +0600

    Finished Rename functionality in context-menu

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,11 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const debug = require('gulp-debug');
+
+const isDevelopment = (process.env.NODE_ENV === 'production') ? false : true;
 
 gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
+        .pipe(debug({title: 'src'}))
+        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
+        .pipe(debug({title: 'sass'}))
+        // .pipe(concat('eye-candy-light.css'))
+        .pipe(debug({title: 'concat'}))
+        .pipe(gulpIf(isDevelopment, sourcemaps.write()))
         .pipe(gulp.dest('css'));
+
+    // Exit from task when all finished
+    callback();
 });
 
 gulp.task('watch', function () {
--- app/js/contextmenu.js
+++ app/js/contextmenu.js
@@ -25,10 +25,44 @@ jQuery(document).ready(function($) {
     // Rename
     $('body').on('click', '.context-rename', function() {
         const elemId = $(this).parents('nav').data('elem-id');
-        $('#'+elemId).html(document.getElementById(elemId).childNodes[0]).append('Привет');
+        $('body').append(`
+            <div class="rename-popup">
+                <div class="dark-bg-popup"></div>
+                <div class="rename-popup-inner">
+                    <i class="app-icons app-icons-small i-close"></i>
+                    <input class="input-general" type="text" value="` + document.getElementById(elemId).innerText + `">
+                    <button class="ok-btn">OK</button>
+                </div>
+            </div>
+        `);
+        $('.rename-popup-inner input').select();
+
+        // Confirm rename operation
+        $('.rename-popup-inner .ok-btn').on('click', function() {
+            const value = $('.rename-popup-inner input').val();
+            $('#'+elemId).html(document.getElementById(elemId).childNodes[0]).append(value);
+            $('.rename-popup').remove();
+        });
+        $('.rename-popup-inner input').keyup(function(event){
+            if (event.keyCode == 13){
+                const value = $('.rename-popup-inner input').val();
+                $('#'+elemId).html(document.getElementById(elemId).childNodes[0]).append(value);
+                $('.rename-popup').remove();
+            }
+        });
+
     });
 
-    // $('body').on('set_text.jstree', function() {
-    //     console.log(123);
-    // });
+    // Close rename popup
+    $('body').on('click', '.rename-popup-inner i, .dark-bg-popup', function() {
+        $('.rename-popup').remove();
+    });
+
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Escape' || event.keyCode === 27) {
+            if ( $('.rename-popup').length > 0 ) {
+                $('.rename-popup').remove();
+            }
+        }
+    });
 });
